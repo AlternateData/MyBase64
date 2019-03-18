e04658021f1fe9790c6ff71e32bc0d8bcc74dd01
Louis Kronberg <louiskronberg@gmail.com>
2019-03-18 20:55:07 +0100

    added some first constants and prototypes

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
+const char * b64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+char * encode(char*, size_t);
+char * padd(char* msg, size_t padding);
+
 
 int main(int argc, char ** argv){
     /* read str from stdout and de/encode */
+    printf("Length of b46_alphabet: %li\n", strlen(b64_alphabet));
     return 0;
 
 }

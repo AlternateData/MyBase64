1055e2e1ca6494ca34928f52c49eddc38654e391
Louis Kronberg <louiskronberg@gmail.com>
2019-04-11 15:35:01 +0200

    added base16 test to test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,16 @@
 set -e 
 # taken from https://tools.ietf.org/html/rfc4648
 TEST_VECTORS=("" "f" "fo" "foo" "foob" "fooba" "foobar")
+echo "============="
 echo "Base 64 Tests"
 echo "============="
 for v in ${TEST_VECTORS[@]}; do
     echo "BASE64($v) =  $(./base64 -e -s $v)";
+done
+
+echo "============="
+echo "Base 16 Tests"
+echo "============="
+for v in ${TEST_VECTORS[@]}; do
+    echo "BASE64($v) =  $(./base64 -e -h -s $v)";
 done

f6d094f525f56971f91f6f9bd2c1c7b6b77d143a
Louis Kronberg <louiskronberg@gmail.com>
2021-01-04 12:53:59 +0100

    refactor and remove dependency on err.h
    errx is best used after errno is set but in my code errx is never used
    anywhere where errno is set. Thus replace it with my own code.

--- main.c
+++ main.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <err.h>
 
 const char * alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-#define die(...) errx(EXIT_FAILURE, __VA_ARGS__)
+#define die(...)    do{                                 \
+                        fprintf(stderr, __VA_ARGS__);   \
+                        exit(EXIT_FAILURE);             \
+                    }while(0)
 
 char map(char d){
-    if( d == '+')
-        return 62;
-    if( d == '/')
-        return 63;
     if('A' <=  d && d <= 'Z')
         return d - 'A';
     if('a' <=  d && d <= 'z')
         return d - 'a' + 26;
     if('0' <=  d && d <= '9')
         return d - '0' + 52;
+    if(d == '+')
+        return 62;
+    if(d == '/')
+        return 63;
     return -1;
 }
 
-void encode_block(char * block,char a, char b, char c){
+void encode_block(char * block, char a, char b, char c){
         block[0] = alphabet[a >> 2];
         block[1] = alphabet[((a & 0x3) << 4) | (b >> 4)];
         block[2] = alphabet[((b & 0xf) << 2) | (c >> 6)];
@@ -48,7 +50,7 @@ char * encode(char *in){
 
     char * out = malloc((outlen+1) * sizeof(char));
     char * block = out;
-    for(; i < (inlen / 3) * 3; i += 3, block+=4)
+    for(; i < (inlen / 3) * 3; i += 3, block += 4)
         encode_block(block, in[i], in[i + 1], in[i + 2]);
 
     if(inlen % 3 == 2){
@@ -58,7 +60,7 @@ char * encode(char *in){
 
     if(inlen % 3 == 1){
         encode_block(block, in[i], 0, 0);
-        out[outlen-2] =  out[outlen-1] = '=';
+        out[outlen-2] = out[outlen-1] = '=';
     }
 
     out[outlen + 1] = '\0';
@@ -72,15 +74,15 @@ char* decode(char * in){
     int32_t padding = 0, i = 0;
 
     if(inlen % 4 || inlen == 0)
-        die("Invalid Message Length. Must be greater than 0 and divisible by 4");
+        die("Invalid Message Length. Must be greater than 0 and divisible by 4\n");
 
     while(in[inlen - 1 - padding] == '=')
         padding++;
+    outlen -= padding;
 
     if(padding > 2)
-        die("%i padding characters were found. That is too much.", padding);
+        die("%i padding characters were found. That is too much.\n", padding);
 
-    outlen -= padding;
     char * out = malloc(outlen * sizeof(char));
     char * block = out;
 
@@ -88,7 +90,7 @@ char* decode(char * in){
         in[j] = map(in[j]);
         if(in[j] == -1){
             free(out);
-            die("Invalid character at %i", j);
+            die("Invalid character at %i\n", j);
         }
     }
 
@@ -107,7 +109,7 @@ char* decode(char * in){
 
 int main(int argc, char * argv[]){
     if(argc != 3)
-        die("Wrong Usage");
+        die("Wrong Usage\n");
 
     char * input = argv[2];
     char * out;
@@ -117,7 +119,7 @@ int main(int argc, char * argv[]){
     }else if(!strcmp(argv[1], "encode")){
         out = encode(input);
     }else{
-        die("Wrong Usage");
+        die("Wrong Usage\n");
     }
 
     printf("%s\n", out);
